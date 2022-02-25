f71dbb8222748f869d48d21ba72fab14e2ae90a8
Matthew <62276229+mattshu0410@users.noreply.github.com>
2022-02-25 11:36:23 +1100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/5bxjoyrlktn3e26/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/u6p2aj6h2bz10jp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
 classes = ['Acne', 'Tret_Burn']
